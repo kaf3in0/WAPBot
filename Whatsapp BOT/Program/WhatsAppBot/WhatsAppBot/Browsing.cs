57c8e7d8dce65c1cedaa63c30b2eb2dd912cae74
Edward <kaf3in0@gmail.com>
2018-04-16 19:43:34 +0300

    Add files via upload

---
+++ Whatsapp BOT/Program/WhatsAppBot/WhatsAppBot/Browsing.cs
@@ -0,0 +1,151 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+// TODO: Clasa asta ar trebui sa se ocupe de accesari, trimitere de mesaje, INTERACTIUNI, NAVIGATIE
+namespace WhatsAppBot
+{
+    
+    public enum EMsgType { TEXT, REPLY, IMAGE, VIDEO, SOUND, NOTFOUND };
+    public class Mesage
+    {
+        public Mesage(EMsgType msgType, string msginfo, string sender, string msg, string repliedTo)
+        {
+            this.msginfo = msginfo;
+            this.sender = sender;
+            this.msg = msg;
+            this.repliedTo = repliedTo;
+            this.msgType = msgType;
+        }
+        public string sender { get; private set; }
+        public string msg { get; private set; }
+        public string msginfo { get; private set; }
+        public string repliedTo { get; private set; }
+        public string target { get; private set; }
+        public EMsgType msgType;
+    }
+
+    public static class Act
+    {
+        public static void hover(this IWebElement webElement, IWebDriver driver)
+        {
+            new Actions(driver).MoveToElement(webElement).Perform();
+        }
+    }
+    public class Browsing
+    {
+        public void Timer(int miliseconds)
+        {
+            System.Threading.Thread.Sleep(miliseconds);
+        }
+        IWebDriver driver;
+        public string getTargetName()
+        {//  //*[@id = 'main']//span[contains(@title,'')]
+            string targetname = driver.FindElement(By.XPath("//*[@id = 'main']//div[contains(@class,'1WBXd')]//span[@title]")).Text;
+            return targetname;
+        }
+        // Constructor
+        public Browsing()
+        {
+            driver = new ChromeDriver("C:\\Users\\Edward\\Documents\\Desktop\\VS COD");
+            driver.Url = "https://web.whatsapp.com/";
+        }
+        public void StarMsg(IWebElement element)
+        {
+            Act.hover(element, driver);
+            try
+            {
+                element.FindElement(By.XPath(".//div[@data-js-context-icon='true']")).Click(); // Click dropdown
+                Timer(300);
+                driver.FindElement(By.XPath(".//div[@class='_3lSL5 _2dGjP _1vu-E'][contains(text(),'Star message')]")).Click();    //Click starmsg
+            }
+            catch
+            {
+                Console.WriteLine("Failed to star msg");
+            }
+            
+        }
+        //private void ReadMsg(IWebElement element)
+        //{
+        //}
+        // Gets you to the group chat or friend conversation specified
+        public void accesTarget(string target)
+        {
+            IWebElement searchTarget = driver.FindElement(By.XPath("//label[@class='_2MSJr']"));
+            searchTarget.SendKeys(target + Keys.Enter);
+        }
+        public IWebElement getChat()
+        {
+            IWebElement chat = driver.FindElement(By.XPath("//div[@class='_2S1VP copyable-text selectable-text']"));
+            return chat;
+        }
+        public void sendMsgTo(string msg, string target, int count = 1)
+        {
+            accesTarget(target);
+            IWebElement chat = getChat();
+            for (int i = 0; i < count; i++){
+                chat.SendKeys(msg + Keys.Enter);
+                System.Threading.Thread.Sleep(600);
+            }
+        }
+        private EMsgType getMsgType(IWebElement msgextended)
+        {
+
+            try {
+                IWebElement soundmsg = msgextended.FindElement(By.XPath(".//input[contains(@type,'range')]"));
+                return EMsgType.SOUND;          
+            }
+            catch { /* Just keep checking */ }
+            // Verifica daca poti primi elementul specific al TIPULUI de mesaj
+            // ex: UN mesaj de tip imagine va avea elementul specific pt imagine, dc nu atunci nu este imagine
+            try {
+                IWebElement imagemsg = msgextended.FindElement(By.XPath(".//img"));
+                return EMsgType.IMAGE;              //TODO: Also dowload the image with this path
+            }
+            catch { /* Just keep checking */ }
+
+            ////*[@id='main']//div[contains(@class,'message')]//div[contains(@class,'Y9G3K')]
+            try
+            {
+                IWebElement videomsg = msgextended.FindElement(By.XPath(".//div[contains(@class,'video')]"));
+                return EMsgType.VIDEO;
+            }
+            catch { /* Just keep checking */ }
+
+            try {
+                IWebElement replymsg = msgextended.FindElement(By.XPath(".//span[contains(@class,'quoted')]"));
+                return EMsgType.REPLY;
+            }
+            catch { /* Just keep checking */ }
+            return EMsgType.TEXT;
+        }
+
+
+        public void GetLastMsg(string target)
+        {
+
+            IList<IWebElement> msg = driver.FindElements(By.XPath("//*[@id='main']//div[contains(@class,'message')]"));
+            IWebElement lastmsg = msg[msg.Count - 1];
+            try
+            {
+                IWebElement star = lastmsg.FindElement(By.XPath(".//span[contains(@data-icon,'star')]"));
+            }
+            catch
+            {
+                try { Console.WriteLine(lastmsg.Text); }
+                catch { }
+                StarMsg(lastmsg);
+            }
+            
+            
+            
+        }
+    }
+}
---
+++ Whatsapp BOT/Program/WhatsAppBot/WhatsAppBot/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+// TODO: Aici ar trebui sa se execute logica pentru rularea BOTului
+namespace WhatsAppBot
+{
+    class Program
+    {
+        static string targetcmd = "!target";
+        static string sendmsgtocmd = "!sendmsgto";
+        static string helpcmd = "!help";
+        
+  
+        static void Main(string[] args)
+        {
+            Browsing browse = new Browsing();
+            while (true)
+            {
+                string input = Console.ReadLine();
+                AdminComand(ref browse, input);
+                browse.accesTarget(input);
+                while(true)
+                    browse.GetLastMsg(input);
+            }
+            
+        }
+        static void printMesages(List<Mesage> m)
+        {
+            for (int i = 0; i < m.Count; i++)
+            {
+                Console.WriteLine(m[i].msg);
+                Console.WriteLine(m[i].repliedTo);
+                Console.WriteLine(m[i].sender);
+                Console.ForegroundColor = ConsoleColor.Red;
+                switch (m[i].msgType)
+                {
+
+                    case EMsgType.TEXT:
+                        Console.WriteLine(EMsgType.TEXT.ToString());
+                        break;
+                    case EMsgType.IMAGE:
+                        Console.WriteLine(EMsgType.IMAGE.ToString());
+                        break;
+                    case EMsgType.VIDEO:
+                        Console.WriteLine(EMsgType.VIDEO.ToString());
+                        break;
+                    case EMsgType.REPLY:
+                        Console.WriteLine(EMsgType.REPLY.ToString());
+                        break;
+                    case EMsgType.SOUND:
+                        Console.WriteLine(EMsgType.SOUND.ToString());
+                        break;
+                }
+                Console.ResetColor();
+                Console.WriteLine();
+            }
+        }
+
+        static void AdminComand(ref Browsing browse, string comand)
+        {
+            if (comand.Contains(targetcmd))
+            {
+                //Remove the comand name before looking for contacts/groups/target
+                string target = comand.Remove(comand.IndexOf(targetcmd), targetcmd.Length + 1);//+1 Because there is a space in front
+                Console.WriteLine(target);
+                browse.accesTarget(target);
+            }
+            else if (comand.Contains(sendmsgtocmd)) // Ex: !sendmsgto ana are mere -Nimic -5
+            {
+                bool skipWhile = false;
+
+                string buffer = comand.Remove(comand.IndexOf(sendmsgtocmd), sendmsgtocmd.Length + 1);//+1 Because there is a space in front
+                Console.WriteLine();
+                int space1 = buffer.IndexOf('-');                 // Find first -
+                int space2 = buffer.IndexOf('-', space1 + 1);       // Find second -, using space1+1 as the starting position
+                if (space2 == -1)       // The function return -1 if substring not found
+                {
+                    space2 = buffer.IndexOf(' ', space1 + 1);
+                    skipWhile = true;
+                }
+                if (space2 == -1)
+                {
+                    space2 = buffer.Length - space1;
+                    skipWhile = true;
+                }
+                string msg = buffer.Substring(0, space1);           // space1 in cazul asta este marimea substringului
+                string target = buffer.Substring(space1 + 1, space2 - space1 - 1);
+
+                space2++;
+                int times = 1;
+                if (skipWhile == false)
+                {
+                    while (buffer[space2] == ' ' || buffer[space2] > '9' || buffer[space2] < '1') { space2++; }
+                    times = buffer[space2] - 48;
+                }
+                Console.WriteLine(space2);
+                //Console.WriteLine(buffer[space2]);
+                Console.WriteLine(times);
+                browse.sendMsgTo(msg, target, times);
+            }
+        }
+    }
+
+
+}
